34f07e1c56ce69e958c8f6dd3f4b83c16e1c87d0
shahriar021 <shahriarc19@gmail.com>
2023-08-19 00:53:19 +0600

    added

---
+++ app/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <div className='bg-black h-20 text-white text-lg flex justify-between items-center '>
+        <div className='m-5'>logo</div>
+        <div>logo</div>
+        <div>logo</div>
+        <div>logo</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
